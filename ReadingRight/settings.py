9f8899ba4a1c2e0134304329ba6ab4ccc7a3b906
dhruv354 <dhruvsin@iitk.ac.in>
2021-09-18 15:49:34 +0530

    did collectstatic

--- ReadingRight/settings.py
+++ ReadingRight/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
+# STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static/'),)
